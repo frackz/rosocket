c2fcb037a96b173270c1d7083f4a4f445187cdd0
frackz <65610875+frackz@users.noreply.github.com>
2023-04-14 08:00:41 +0200

    sad

--- place/src/Socket.lua
+++ place/src/Socket.lua
@@ -17,7 +17,7 @@ type ConnectionFailure = {
 
 }
 
---- Connect to a websocket by URL
+--- Connect to a websocket using a URL.
 function Socket:Connect(url : string, host : string): Connection | ConnectionFailure
     local Response = HttpService:PostAsync(host .. "/connect", HttpService:JSONEncode({
         url = url,
@@ -28,7 +28,9 @@ end
 
 --- Send a HTTP request through a host (Using axios). Which can act as a proxy.
 function Socket:Send(data : table, host : string)
-    
+    local Response = HttpService:PostAsync(host .. "/send", HttpService:JSONEncode({
+        
+    }), ApplicationJson)
 end
 
 return Socket
--- place/src/init.server.lua
+++ place/src/init.server.lua
@@ -1,5 +1,5 @@
 local WebSocket = require(script.Socket)
 
-WebSocket:Connect('asd', 'https://prevailinggigafolder.bertil123.repl.co')
+WebSocket:Connect()
 
 return WebSocket
