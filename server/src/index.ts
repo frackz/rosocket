c9ffdc6244214eec8cfd2d20e024b016bc97639c
frackz <65610875+frackz@users.noreply.github.com>
2023-04-18 09:48:07 +0200

    changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,4 +10,4 @@ export var sockets: Object = {}
 app.use(express.json())
 app.use('/', Routes)
 
-app.listen(port, () => { console.log("Started server on port "+port) })
+app.listen(port, () => console.log("Started server on port "+port))
